0685b82b75fd43984944aae58abf7823f4a79020
Alex Mihov <alexmihov.as@gmail.com>
2025-02-18 16:56:32 +0100

    [feat] milestone 2

--- src/components/Main/ListaLibri.jsx
+++ src/components/Main/ListaLibri.jsx
@@ -1,28 +1,67 @@
+import { useState } from 'react';
+
 const titoliLibri = [
-    "1984 - George Orwell",
-    "Il Signore degli Anelli - J.R.R. Tolkien",
-    "Orgoglio e Pregiudizio - Jane Austen",
-    "Il Grande Gatsby - F. Scott Fitzgerald",
-    "Don Chisciotte - Miguel de Cervantes",
-    "Harry Potter e la Pietra Filosofale - J.K. Rowling",
-    "Uno, Nessuno e Centomila - Luigi Pirandello",
-    "Moby Dick - Herman Melville",
-    "La Divina Commedia - Dante Alighieri",
-    "Guerra e Pace - Lev Tolstoj"
+    "1984",
+    "Il Signore degli Anelli",
+    "Orgoglio e Pregiudizio",
+    "Il Grande Gatsby",
+    "Don Chisciotte",
+    "Harry Potter e la Pietra Filosofale",
+    "Uno, Nessuno e Centomila",
+    "Moby Dick",
+    "La Divina Commedia",
+    "Guerra e Pace"
 ];
 
 
 export default function ListaLibri() {
 
+    // stato lista libri
+    const [listaTitoli, setListaTitoli] = useState(titoliLibri);
+
+    // stato dell'input di inserimento del nuovo libro
+    const [nuovoTitolo, setNuovoTitolo] = useState("")
+
+    // funzione di callback che aggiuge l'elemento alla lista
+    const aggiungiLibro = e => {
+        // preveniamo il funzionamento di base del submit
+        e.preventDefault();
+
+        // creazione nuovo array che contiene la vacchia lista più il nuovo libro
+        const listaAggiornata = [...listaTitoli, nuovoTitolo];
+
+        // aggiorniamo la lista 
+        setListaTitoli(listaAggiornata);
+
+        // azzeriamo il valore di nuovo libro
+        setNuovoTitolo("");
+    }
+
     return (
-        <div>
-            <h1>Lista Libri Famosi</h1>
-            <ul>
-                {titoliLibri.map((title, index) => (
-                    <li key={index}>{title}</li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <h1>Lista Libri</h1>
+
+            <form onSubmit={aggiungiLibro}>
+                <input type='text' value={nuovoTitolo} placeholder='Inserisci Titolo Libro'
+                    onChange={e => setNuovoTitolo(e.target.value)}
+                />
+                <button type='submit'>AGGIUNGI</button>
+            </form>
+
+
+            {/* lista dei libri */}
+            <div>
+                <ul>
+                    {listaTitoli.map((title, index) => (
+                        <li key={index}>{title}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
+
+
+
+
     );
 
 };
